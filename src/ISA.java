4e8cd20fd84a4230a7fa5df1ce5312406fc8750c
cam <cameron.prayle@gmail.com>
2022-05-18 14:43:53 +0100

    added ISA/Buisness classes

--- src/ISA.java
+++ src/ISA.java
@@ -3,10 +3,10 @@ public class ISA extends Account{
     public ISA(String name, String address, String email, int age, float balance) {
         super(name, address, email, age, balance);
         this.sortCode = "24-65-69";
-        this.interest=2.0f;
+        this.interest=0.02f;
     }
 
     public void addInterest(){
-        this.balance = this.balance * (1 + this.interest/10);
+        this.balance = (this.balance * this.interest) + this.balance;
     }
 }
